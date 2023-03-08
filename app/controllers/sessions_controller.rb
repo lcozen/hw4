369b9cbeceddbf4f991c68d53b2090576de596f7
lcozen <lindsay.cozen@kellogg.northwestern.edu>
2023-03-08 17:35:53 +0000

    starter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #check if user exists
+    @user = User.find_by({"email"=>params["email"]})
+    if @User
+    #if they do, check that they know password
+      #if params["password"] == @user["password"]
+    if BCrypt::Password.new(@user["password"]) == params["password"]
+    #if they do, send them in
+        redirect_to "/places"
+      else
+        redirect_to "/"
+      end 
+    else 
+      redirect_to "/"
+    end
+
   end
 
   def destroy
